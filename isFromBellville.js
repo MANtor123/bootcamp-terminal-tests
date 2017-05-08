5b3244d26646d6506e219cb2286d329e72d9923f
Mponeng <mantombiratego@gmail.com>
2017-05-08 16:08:35 +0200

    added function

---
+++ isFromBellville.js
@@ -0,0 +1,3 @@
+module.export = function(regNr){
+  return isFromBellville;
+}
---
+++ test/isFromBellville.test.js
@@ -0,0 +1,14 @@
+
+var assert = require ('assert')
+var isFromBellville = require('../isFromBellville.js')
+
+describe('The isFromBellville function', function(){
+
+    it('should find out if the parameter passed in is a reg number from paarl', function(){
+        assert.equal(isFromBellville('CY'), true);
+    });
+    it('should find out if the parameter passed in is a reg number from paarl', function(){
+       assert.equal(isFromBellville('CF'), false);
+
+    });
+});

38fe207788047a122c809e6fd66a8cd2d1bad2e8
Mponeng <mantombiratego@gmail.com>
2017-05-15 14:15:51 +0200

    modified files

--- test/fromWhere.test.js
+++ test/fromWhere.test.js
@@ -3,20 +3,20 @@ var assert = require('assert')
 
 describe('the fromWhere function', function(){
   it('should find out if the parameter passed in is a reg number from Bellville', function(){
-          assert.equal(fromWhere('CY'), true);
+          assert.equal(fromWhere('CY'),'Bellville');
       });
 
       it('should find out if the parameter passed in is a reg number from Paarl', function(){
-         assert.equal(fromWhere('CJ'), true);
+         assert.equal(fromWhere('CJ'),'Paarl');
 
       });
 
   	it('should find out if the parameter passed in is a reg number from Cape Town', function(){
-         assert.equal(fromWhere('CA'), false);
+         assert.equal(fromWhere('CA'),'Cape Town');
 
       });
   	it('should find out if the parameter passed in is a reg number from some other place!', function(){
-         assert.equal(fromWhere('CL'), false);
+         assert.equal(fromWhere('CL'),"Some other place!");
 
       });
 })
